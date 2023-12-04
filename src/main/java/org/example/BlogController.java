e41c07ebf89e1ad4203545c1b456866102943633
choeun7 <short0720@gmail.com>
2023-12-04 17:09:37 +0900

    application.yaml에 커스텀 설정 쓰고 읽기

--- src/main/java/org/example/BlogController.java
+++ src/main/java/org/example/BlogController.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogController {
+    @Value("${mju.blog.articles_per_page}")
+    int articlePerPage;
+
     @GetMapping("/hello")
     public String hello() {
-        return "world";
+        return "world" + articlePerPage;
     }
 }
