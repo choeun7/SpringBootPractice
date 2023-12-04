91fff82886f77ddef6803ced1819a725defc646e
choeun7 <short0720@gmail.com>
2023-12-04 17:30:51 +0900

    @RequestBody

--- src/main/java/org/example/BlogController.java
+++ src/main/java/org/example/BlogController.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @RestController
 public class BlogController {
@@ -38,5 +43,24 @@ public class BlogController {
         r.title = "즐거운 하루";
         return r;
     }
+
+//    @PostMapping("/article")
+//    public void postArticle(HttpServletRequest request, HttpServletResponse response)
+//            throws IOException {
+//        System.out.println(request.getMethod());
+//
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8));
+//        String payload = in.lines().collect(Collectors.joining());
+//        System.out.println(payload);
+//        response.addHeader("MY_HEADER", "MY_VALUE");
+//        response.getOutputStream().println("Hello");
+//    }
+
+    @PostMapping("/article")
+    public void postArticle(@RequestBody PostArticleRequest body) {
+        System.out.println(body.title);
+    }
+
 
 }
