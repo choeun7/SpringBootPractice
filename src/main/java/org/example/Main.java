a713c9431d777cdd58213569fb8de3c5ee7a93e0
choeun7 <short0720@gmail.com>
2023-12-04 16:29:08 +0900

    @SpringBootApplication로 태깅된 클래스

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    // 진입점
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
